e7388d174a8212a0d723a529719f7d6823a82e9d
vicky <vcie2210@gmail.com>
2024-01-30 11:27:52 +0800

    加上api控制器內的功能

--- ResfulPractice/Controllers/ApiController.cs
+++ ResfulPractice/Controllers/ApiController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using ResfulPractice.Models;
+using System.Text;
 
 namespace ResfulPractice.Controllers
 {
@@ -14,7 +15,7 @@ namespace ResfulPractice.Controllers
         }
         public IActionResult Index()
         {
-            return View();
+            return Content("Hello Fetch!!", "text/plain", Encoding.UTF8);
         }
 
         public IActionResult Cities()
@@ -23,6 +24,19 @@ namespace ResfulPractice.Controllers
             var cities= _context.Addresses.Select(x => x.City).Distinct();  
             return Json(cities);
         }
-
+        public IActionResult Avatar(int id = 1)
+        {
+            //不用_context.Member.Where(m=>m.MemberId=id) 因為Find會自動根據主所以見搜尋
+            Member? member = _context.Members.Find(id);
+            if (member != null)
+            {
+                byte[] img = member.FileData;
+                if (img != null)
+                {
+                    return File(img, "image/jpeg");
+                }
+            }
+            return NotFound();
+        }
     }
 }
